ed3b018caf797332a948b7bd2ff2e5ab7c9f49e4
KseniTi <lioness2006@bk.ru>
2022-12-08 17:09:23 +0300

    Prog041

---
+++ Prog041/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3
+*/
+
+void Zadacha41()
+{
+    /*Console.WriteLine("Введите число");
+    string numbers = Console.ReadLine();*/
+
+    int[] numbers = {0, 7, 8, -2, -2};
+
+    int summ = 0;
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        if (numbers[i] > 0) summ++;
+    }
+    Console.WriteLine(summ);
+}
+
+
+Zadacha41();
